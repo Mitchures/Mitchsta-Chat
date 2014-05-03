7ed96dea3d803294bbb3e560b3c562f4b5ae03f8
Mitchell Hollander <MitchellSH@live.com>
2014-05-02 22:59:34 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+/* global Firebase:false */
+/* exported Firebase */
+
+
+(function(){
+
+  'use strict';
+
+  var myDataRef = new Firebase('https://radiant-fire-128.firebaseio.com');
+
+  $(document).ready(initialize);
+
+
+
+
+  function initialize(){
+    $('#messageInput').keypress(EnterInput);
+    myDataRef.on('child_added', pullData);
+    $('#nameInput').focus();
+  }
+
+
+
+  function EnterInput(e){
+    if (e.keyCode === 13) {
+      var name = $('#nameInput').val();
+      var text = $('#messageInput').val();
+      myDataRef.push({name: name, text: text});
+      $('#messageInput').val('');
+      $('#messageInput').focus();
+    }
+  }
+
+  function pullData(snapshot){
+    var message = snapshot.val();
+    displayChatMessage(message.name, message.text);
+
+    console.log(message.name);
+    console.log(chatNames);
+  }
+
+  function displayChatMessage(name, text) {
+    $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+    $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+  };
+
+})();
